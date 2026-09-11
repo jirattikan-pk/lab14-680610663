3b65272d99600a0f901d9b92f41eae5183248f2e
jirattikan-pk <mindmook.2006@gmail.com>
2026-09-11 23:40:15 +0700

    cp

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,49 @@
 import UserRegisterCard from "../components/UserRegisterCard";
 import type { Registrant } from "../libs/Registrant";
+import { useState, useEffect } from "react";
+
+const STORAGE_KEY = "lap14";
+const defaultTasks: Registrant[] = [];
+
+function loadTasks(): Registrant[] {
+  try {
+    
+    const raw = localStorage.getItem(STORAGE_KEY);
+    return raw ? JSON.parse(raw) : defaultTasks;
+  } catch {
+    return defaultTasks; // เผื่อข้อมูลใน localStorage เสีย
+  }
+}
 
 export default function DashboardPage() {
+  const [registrant, setRegistrant] = useState<Registrant[]>(loadTasks);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(registrant));
+  }, [registrant]);
+
   return (
     <div className="container mt-4">
       <h2>Dashboard</h2>
-      {/* Conditional Rendering + Render Component */}
+      <div className="mt-3">
+        <label className="form-label">
+          ผู้ลงทะเบียนแล้ว ({registrant.length} คน)
+        </label>
+          <div className="d-flex flex-column gap-2">
+          {registrant.map((regist) =>(
+            <UserRegisterCard
+            key={regist.id}
+            id={regist.id}
+            fullName={regist.fullName}
+            gender={regist.gender}
+            plan={regist.plan}
+            total={regist.total}
+            extraItems={regist.extraItems}
+            />
+          ))}
+          </div>
+        
+      </div>
     </div>
   );
 }
